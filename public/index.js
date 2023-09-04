c8260daaf80e12788395bb5525b98784534056d5
John <jjpyae@gmail.com>
2023-09-04 15:00:07 -0700

    completes day 1

--- public/index.js
+++ public/index.js
@@ -1,1 +1,27 @@
-// Your code here
+window.onload = loadCat;
+
+async function loadCat() {
+  const h1 = document.createElement('h1');
+  const img = document.createElement('img');
+
+  h1.textContent = "Cat";
+
+  getCatPic()
+  .then(catPic => {
+    img.setAttribute('src', catPic);
+  });
+
+  document.body.appendChild(h1);
+  document.body.appendChild(img);
+}
+
+function getCatPic() {
+  try {
+    const response = fetch("https://api.thecatapi.com/v1/images/search")
+    .then(res => res.json())
+    .then(resBody => resBody[0].url);
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
