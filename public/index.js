e041b7cfab105e202ee8cfde43bb8d408a532725
John <jjpyae@gmail.com>
2023-09-05 15:50:34 -0700

    Refactored day 1 and day 2 codes

--- public/index.js
+++ public/index.js
@@ -1,6 +1,4 @@
-window.onload = loadCat;
-
-async function loadCat() {
+document.addEventListener("DOMContentLoaded", () => {
   const [h1, newPic, img, popularityScore] = createElement('h1', 'button', 'img', 'p');
 
   //div container for upvote and downvote buttons
@@ -11,102 +9,118 @@ async function loadCat() {
 
   const commentDisplay = document.createElement('div');
 
-  getCatPic()
-  .then(catPic => {
-    img.setAttribute('src', catPic);
-  });
+  async function loadCat() {
+    addTextContentToTheElements();
 
-  h1.textContent = "Catstagram";
-  newPic.textContent = "Get A New Cat Image";
-  upVote.textContent = "Upvote";
-  downVote.textContent = "Downvote";
-  submit.textContent = "Submit";
-  popularityScore.textContent = "Popularity Score: 0";
-  commentLabel.textContent = "Comment:";
+    h1.style.padding = "20px";
 
-  h1.style.padding = "20px";
+    setAttributesToTheElements();
 
-  popularityScore.setAttribute("id", "score");
-  newPic.setAttribute("id", "new-image");
-  divClicks.setAttribute("id", "vote");
-  divComment.setAttribute("id", "comment-section");
-  commentLabel.setAttribute("for", "comment")
-  commentField.setAttribute("type", "text");
-  commentField.setAttribute("name", "comment");
-  commentField.setAttribute("id", "comment");
-  commentField.setAttribute("placeholder", "Add a comment...");
-  commentDisplay.setAttribute("id", "comment-display");
+    addEventListenerToTheButtons();
 
-  upVote.addEventListener("click", changeScore);
-  downVote.addEventListener("click", changeScore);
-  submit.addEventListener("click", addComment);
-  newPic.addEventListener("click", getNewImage);
+    appendChildToTheParents();
 
-  document.body.appendChild(h1);
-  document.body.appendChild(newPic);
-  document.body.appendChild(img);
-  document.body.appendChild(popularityScore);
-  divClicks.appendChild(upVote);
-  divClicks.appendChild(downVote);
-  document.body.appendChild(divClicks);
-  divComment.appendChild(commentLabel);
-  divComment.appendChild(commentField);
-  divComment.appendChild(submit);
-  document.body.appendChild(divComment);
-  document.body.appendChild(commentDisplay);
-}
-
-function createElement(...elements) {
-  elements.forEach((element, index) => {
-    elements[index] = document.createElement(element);
-  })
-  return elements;
-}
-
-function getCatPic() {
-  try {
-    const response = fetch("https://api.thecatapi.com/v1/images/search")
-    .then(res => res.json())
-    .then(resBody => resBody[0].url);
-    return response;
-  } catch (error) {
-    console.error("Error:", error);
   }
-}
 
-function changeScore(e) {
-  const scoreNode = document.querySelector("#score");
-  let currScore = Number(scoreNode.textContent.split(": ")[1]);
-  if (e.target.textContent === "Upvote") {
-    currScore++;
-    scoreNode.textContent = `Popularity Score: ${currScore}`;
-  } else if (e.target.textContent === "Downvote" && currScore > 0) {
-    currScore--;
-    scoreNode.textContent = `Popularity Score: ${currScore}`;
+  loadCat();
+
+  //Helper functions
+
+  //Creates Elements
+  function createElement(...elements) {
+    elements.forEach((element, index) => {
+      elements[index] = document.createElement(element);
+    })
+    return elements;
   }
-}
 
-function addComment(e) {
-  const inputNode = document.querySelector("#comment");
-  const displayNode = document.querySelector("#comment-display");
-  const comment = inputNode.value;
-  const commentDiv = document.createElement('div');
-  commentDiv.textContent = comment;
-  displayNode.appendChild(commentDiv);
+  //Adds textContent to the elements
+  function addTextContentToTheElements() {
+    h1.textContent = "Catstagram";
+    newPic.textContent = "New Pic";
+    upVote.textContent = "Upvote";
+    downVote.textContent = "Downvote";
+    submit.textContent = "Submit";
+    popularityScore.textContent = "Popularity Score: 0";
+    commentLabel.textContent = "Comment:";
+  }
 
-  console.log(comment);
-}
+  //Sets attributes to the elements
+  async function setAttributesToTheElements() {
+    img.setAttribute('src', await getCatPic());
+    popularityScore.setAttribute("id", "score");
+    newPic.setAttribute("id", "new-image");
+    divClicks.setAttribute("id", "vote");
+    divComment.setAttribute("id", "comment-section");
+    commentLabel.setAttribute("for", "comment")
+    commentField.setAttribute("type", "text");
+    commentField.setAttribute("name", "comment");
+    commentField.setAttribute("id", "comment");
+    commentField.setAttribute("placeholder", "Add a comment...");
+    commentDisplay.setAttribute("id", "comment-display");
+  }
 
-function getNewImage(e) {
-  const img = document.querySelector('img');
-  const scoreNode = document.querySelector("#score");
-  const displayNode = document.querySelector("#comment-display");
+  //Gets url of the random cat picture from the cat API
+  async function getCatPic() {
+    try {
+      const response = await fetch("https://api.thecatapi.com/v1/images/search");
+      const resBody = await response.json();
+      return resBody[0].url;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
-  getCatPic()
-  .then(catPic => {
-    img.setAttribute('src', catPic);
-  });
+  //Gets new cat images and resets popularity score and comments from previous the cat image
+  async function getNewImage(e) {
+    img.setAttribute('src', await getCatPic());
+    popularityScore.textContent = "Popularity Score: 0";
+    commentDisplay.textContent = "";
+  }
 
-  scoreNode.textContent = "Popularity Score: 0";
-  displayNode.textContent = "";
-}
+  //Add Event listeners to the buttons
+  function addEventListenerToTheButtons() {
+    upVote.addEventListener("click", changeScore);
+    downVote.addEventListener("click", changeScore);
+    submit.addEventListener("click", addComment);
+    newPic.addEventListener("click", getNewImage);
+  }
+
+  //Appends Child to the parents
+  function appendChildToTheParents() {
+    document.body.appendChild(h1);
+    document.body.appendChild(newPic);
+    document.body.appendChild(img);
+    document.body.appendChild(popularityScore);
+    divClicks.appendChild(upVote);
+    divClicks.appendChild(downVote);
+    document.body.appendChild(divClicks);
+    divComment.appendChild(commentLabel);
+    divComment.appendChild(commentField);
+    divComment.appendChild(submit);
+    document.body.appendChild(divComment);
+    document.body.appendChild(commentDisplay);
+  }
+
+  //Changes the popularity score based on Upvote and Downvote button
+  function changeScore(e) {
+    let currScore = Number(popularityScore.textContent.split(": ")[1]);
+    if (e.target.textContent === "Upvote") {
+      currScore++;
+    } else if (e.target.textContent === "Downvote" && currScore > 0) {
+      currScore--; //It won't go below 0;
+    }
+    popularityScore.textContent = `Popularity Score: ${currScore}`;
+  }
+
+  //Adds comment submitted;
+  function addComment(e) {
+    const comment = commentField.value;
+    if (comment) {
+      const commentDiv = document.createElement('div');
+      commentDiv.textContent = comment;
+      commentDisplay.appendChild(commentDiv);
+      commentField.value = "";
+    }
+  }
+});
